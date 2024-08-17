209bbed110a38fd419e112e941dbb505e997492a
Zelphy712 <34755709+Zelphy712@users.noreply.github.com>
2024-08-17 18:55:19 -0500

    created audio manager and added a few sounds. also added methods for menu to adjust sounds

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.SceneManagement;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance;
+
+    [SerializeField]
+    public Sound [] musicSounds, sfxSounds;
+    [SerializeField]
+    public AudioSource musicSource, sfxSource;
+
+
+    private void Awake(){
+        if(Instance == null){
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }else{
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start(){
+        // Create a temporary reference to the current scene.
+		Scene currentScene = SceneManager.GetActiveScene ();
+
+		// Retrieve the name of this scene.
+		string sceneName = currentScene.name;
+        if(sceneName == "GameplayScene"){
+            PlayMusic("Level Theme");
+        }else if(sceneName == "StartMenu"){
+            PlayMusic("Title Screen");
+        }
+    }
+
+    public void PlayMusic(string name){
+        Sound s = Array.Find(musicSounds, x => x.soundName == name);
+
+        if(s == null){
+            Debug.Log("Sound " + name + " not found.");
+        }else{
+            musicSource.clip = s.clip;
+            musicSource.Play();
+        }
+    }
+
+    public void PlaySfx(string name){
+        Sound s = Array.Find(sfxSounds, x => x.soundName == name);
+
+        if(s == null){
+            Debug.Log("Sound " + name + " not found.");
+        }else{
+            sfxSource.PlayOneShot(s.clip);
+        }
+    }
+
+    public void ToggleMusic(){
+        musicSource.mute = !musicSource.mute;
+    }
+
+    public void ToggleSfx(){
+        sfxSource.mute = !sfxSource.mute;
+    }
+    public void MusicVolume(float volume)
+    {
+        musicSource.volume = volume;
+    }
+
+    public void SfxVolume(float volume){
+        sfxSource.volume = volume;
+    }
+}
