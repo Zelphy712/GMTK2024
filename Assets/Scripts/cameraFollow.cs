c8f027c8ae55eb8d7a08edc6fddb58584cf3e8a9
Zelphy712 <34755709+Zelphy712@users.noreply.github.com>
2024-08-17 11:48:27 -0500

    created dialogue system

--- Assets/Scripts/cameraFollow.cs
+++ Assets/Scripts/cameraFollow.cs
@@ -7,31 +7,12 @@ public class cameraFollow : MonoBehaviour
     public float FollowSpeed = 2f;
     public Vector3 offset;
     public Transform target;
-    [SerializeField, Range(0f, 100f)]
-    float maxDistance = 10f;
 
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         Vector3 newPos = new Vector3(target.position.x + offset.x,target.position.y + offset.y,-10f);
         transform.position = Vector3.Slerp(transform.position,newPos,FollowSpeed*Time.fixedDeltaTime);
-
-
-
-
-        // distance = Vector2.Distance(transform.position, (target.position + offset));
-        // if(distance > maxDistance){
-        //     newPos = transform.position - (target.position + offset);
-        //     newPos = newPos.normalized * maxDistance + target.position + offset;
-        //     transform.position = newPos;
-        // }
     }
 }
