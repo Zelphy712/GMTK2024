879b6f734d4becc240225dc2cc77a0008fb81b06
Matthew Taylor <matthewtaylor@missouristate.edu>
2024-08-18 15:59:44 -0500

    add enemy behaivior scripts

---
+++ Assets/Scripts/enemy/EnemyDamage.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDamage : MonoBehaviour
+{
+    public int damage;
+    // public PlayerHealth playerHealth;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {        
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            playerState playerState = collision.gameObject.GetComponent<playerState>();
+            if (playerState != null){
+                playerState.TakeDamage(damage);
+            }
+            
+        }
+    }
+
+}
