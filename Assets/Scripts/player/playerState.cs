35fa165a84c174fb9b9974d1d0465824c9aaa452
Zelphy712 <34755709+Zelphy712@users.noreply.github.com>
2024-08-18 13:12:29 -0500

    added more ui stuff!

--- Assets/Scripts/player/playerState.cs
+++ Assets/Scripts/player/playerState.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class playerState : MonoBehaviour
 {
@@ -10,6 +11,33 @@ public class playerState : MonoBehaviour
     private int playerHealth = 6;
     [SerializeField]
     private int playerBones = 0;
+
+    [SerializeField] Image[] healthSkulls;
+    [SerializeField] Image[] litBones;
+    [SerializeField] Image[] unlitBones;
+    
+
+    public void FixedUpdate(){
+        for (int i = 0; i < healthSkulls.Length; i++)
+        {
+            if(i >= playerHealth){
+                healthSkulls[i].enabled = false;
+            }else{
+                healthSkulls[i].enabled = true;
+            }
+        }
+
+        for (int i = 0; i < litBones.Length; i++)
+        {
+            if(i >= playerBones){
+                litBones[i].enabled = false;
+                unlitBones[i].enabled = true;
+            }else{
+                litBones[i].enabled = true;
+                unlitBones[i].enabled = false;
+            }
+        }
+    }
 
     private void OnEnable(){
         Pearls.OnPearlsCollected += CollectPearls;
